1c767b6b43e15701d9b6c014caead497b46b01f3
bergcb <506852280@qq.com>
2018-07-27 14:55:41 +0800

    修改settings.py中的配置，准备传到git

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8od93wxfn(w212tqenf)7gl(sdy0+10*1paoby8gg7!9brspvp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+
+
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.echobobo.xin']
 
 
 # Application definition
@@ -120,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
