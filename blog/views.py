4ce5f4501c260a60f02432854d58d25603ed119c
bergcb <506852280@qq.com>
2018-07-29 15:51:31 +0800

    Markdown 自动生成文章目录，在文章详情页显示目录

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Category,Tag
 from comments.forms import CommentForm
 from django.views.generic import ListView, DetailView
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
 import markdown
 
 
@@ -157,15 +159,26 @@ class PostDetailView(DetailView):
 
         return response
 
+    # def get_object(self, queryset=None):
+    #     # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
+    #     post = super(PostDetailView, self).get_object(queryset=None)
+    #     post.body = markdown.markdown(post.body,
+    #                                   extensions=[
+    #                                       'markdown.extensions.extra',
+    #                                       'markdown.extensions.codehilite',
+    #                                       'markdown.extensions.toc',
+    #                                   ])
+    #     return post
     def get_object(self, queryset=None):
-        # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
         post = super(PostDetailView, self).get_object(queryset=None)
-        post.body = markdown.markdown(post.body,
-                                      extensions=[
-                                          'markdown.extensions.extra',
-                                          'markdown.extensions.codehilite',
-                                          'markdown.extensions.toc',
-                                      ])
+        # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            TocExtension(slugify=slugify),
+        ])
+        post.body = md.convert(post.body)
+        post.toc = md.toc
         return post
 
     def get_context_data(self, **kwargs):
